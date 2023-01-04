a173dd073f4370077fc2c5f6a5779423d2828ebf
dschoi30 <ds0631@gmail.com>
2023-01-04 09:18:34 +0900

    F3_InsertionSort

---
+++ src/main/java/greedyAlgorithm/Main.java
@@ -0,0 +1,13 @@
+package greedyAlgorithm;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Main T = new Main();
+        Scanner kb = new Scanner(System.in);
+
+
+    }
+}
